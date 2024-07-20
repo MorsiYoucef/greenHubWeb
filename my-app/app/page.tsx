8b6c3fe6365503ee3dc8c7719cf9839592f963be
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-07-20 17:26:07 +0100

    new commit

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,10 +4,10 @@ import Button from '@mui/material/Button'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="contained" className=" bg-green-500 font-roboto">
+      <Button href='/sign-up' variant="contained" className=" bg-green-500 font-roboto">
         Hello world
       </Button>
-      <Typography >Welcome to GreenHub!</Typography>
+      <Typography variant='h4' >Welcome to GreenHub!</Typography>
     </main>
   )
 }
